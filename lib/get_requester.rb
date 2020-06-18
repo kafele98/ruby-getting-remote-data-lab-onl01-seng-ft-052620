f6e063d8126471161e5002e384042d31895fdfbd
Zoe Keller <keller.zoe98@gmail.com>
2020-06-18 12:57:38 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,11 +3,11 @@
 class GetRequester 
   
   def initialize(url)
-      @url = URL 
+      @url = url 
   end 
   
   def get_response_body 
-    uri = URI.parse(URL)
+    uri = URI.parse(@url)
     response = Net::HTTP.get_response.uri 
     response.body 
   end
